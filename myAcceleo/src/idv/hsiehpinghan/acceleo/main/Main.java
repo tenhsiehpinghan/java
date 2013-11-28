cee52e8879ee7f9e3eb8c9412b5a7f1baa44ac11
tenhsiehpinghan <ten.hsiehpinghan@gmail.com>
2013-11-28 23:29:55 +0800

    測試。

--- myAcceleo/src/idv/hsiehpinghan/acceleo/main/Main.java
+++ myAcceleo/src/idv/hsiehpinghan/acceleo/main/Main.java
@@ -42,7 +42,7 @@ public class Main extends AbstractAcceleoGenerator {
      *
      * @generated
      */
-    public static final String[] TEMPLATE_NAMES = { "main" };
+    public static final String[] TEMPLATE_NAMES = { "main_test" };
     
     /**
      * The list of properties files from the launch parameters (Launch configuration).
